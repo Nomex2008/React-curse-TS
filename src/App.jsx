2055bf0869ff2f17808b049dc088bc656ef342a7
Alex <97978327+Nomex2008@users.noreply.github.com>
2024-03-07 23:04:55 +0200

    Commit 2
    Add img and map.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import styles from "./App.module.scss";
 
+const menu = [
+  {
+    name:'Home',
+    link:'/',
+  },
+  {
+    name:'Prodacts',
+    link:'/prodacts',
+  },
+]
+
 export function App() {
   return (
     <>
       <div className={styles.hello}>Hello</div>
+      {menu.map(item => (
+        <p key={item.link}>{item.name}</p>
+      ))}
+      <img src="/email.jpg" alt="" />
     </>
   )
 }
